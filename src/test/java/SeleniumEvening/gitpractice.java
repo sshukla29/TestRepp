6fc01d11bb43aeca3ce57a60409f49febcbc39c8
Sunil <sunil.shukla237@gmail.com>
2024-01-09 21:10:19 +0530

    initial commit

--- src/test/java/SeleniumEvening/gitpractice.java
+++ src/test/java/SeleniumEvening/gitpractice.java
@@ -4,5 +4,7 @@ public class gitpractice {
 
 	public static void main(String[] args) {
 		System.out.println("gitTest");
+		System.out.println("Hello India");
+		System.out.println("India...");
 	}
 }
